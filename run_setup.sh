cb8c56f11758b2db3424460f3fc8b18061e96303
chats-bug <chatterjeesukrit@outlook.com>
2024-07-25 14:44:58 +0530

    feat: initial repo setup

---
+++ run_setup.sh
@@ -0,0 +1,6 @@
+# this is where the indices, and persistent states are to be dumped
+mkdir saved_states
+
+# put whatever repos you want to clone here
+# example workspace/SuperAGI, workspace/AGI-Studio
+mkdir workspace
